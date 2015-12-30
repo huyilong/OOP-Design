93b5a4b136438d03e4096533623399a9d8ddc122
hu.yilong <hu.yilong@wustl.edu>
2015-12-30 15:57:41 -0600

    JUnit - unitTest

---
+++ UnitTest.java
@@ -0,0 +1,15 @@
+JUnit is a Regression Testing Framework used by developers to implement 
+unit testing in Java and accelerate programming speed and increase the 
+quality of code. JUnit Framework can be easily integrated with either of 
+
+the followings:
+
+JUnit test framework provides following important features
+Fixtures
+
+Test suites
+
+Test runners
+
+JUnit classes
+
