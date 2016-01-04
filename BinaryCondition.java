cf6908cb4d2f1dd3c37ae0fa46f35f0f0a4d44e4
hu.yilong <hu.yilong@wustl.edu>
2016-01-04 11:05:23 -0600

    SUMMARY - ALLLL **True/False-left** Problems

--- BinaryCondition.java
+++ BinaryCondition.java
@@ -148,3 +148,103 @@ start + 1 < end
 1.和上面大神说的一样，如果有一个target，当nums[mid]==target，就返回的时候，就写成while(lo<=hi)。lo=mi+1.   hi=mid-1
 2.没有有一个target，当nums[mid]==target，需要返回的话。即肯定要把这个array全部都进行binary search（比如刚刚你说的rotate point和我说的那个missing number），就用的是while(lo<hi). lo=mid+1. hi=mid
 我也很弱，具体为什么我也不清楚，只是看别人答案总结的，有可能因为样本不够大，所以是错误的，欢迎大家批评指正！
+
+Union and Intersection of two sorted arrays
+Given two sorted arrays, find their union and intersection.
+
+For example, if the input arrays are: 
+arr1[] = {1, 3, 4, 5, 7}
+arr2[] = {2, 3, 5, 6}
+Then your program should print Union as {1, 2, 3, 4, 5, 6, 7} and Intersection as {3, 5}.
+Algorithm Union(arr1[], arr2[]):
+For union of two arrays, follow the following merge procedure.
+1) Use two index variables i and j, initial values i = 0, j = 0
+2) If arr1[i] is smaller than arr2[j] then print arr1[i] and increment i.
+3) If arr1[i] is greater than arr2[j] then print arr2[j] and increment j.
+4) If both are same then print any of them and increment both i and j.
+5) Print remaining elements of the larger array.
+
+#include<stdio.h>
+ 
+/* Function prints union of arr1[] and arr2[]
+   m is the number of elements in arr1[]
+   n is the number of elements in arr2[] */
+int printUnion(int arr1[], int arr2[], int m, int n)
+{
+  int i = 0, j = 0;
+  while (i < m && j < n)
+  {
+    if (arr1[i] < arr2[j])
+      printf(" %d ", arr1[i++]);
+    else if (arr2[j] < arr1[i])
+      printf(" %d ", arr2[j++]);
+    else
+    {
+      printf(" %d ", arr2[j++]);
+      i++;
+    }
+  }
+ 
+  /* Print remaining elements of the larger array */
+  while(i < m)
+   printf(" %d ", arr1[i++]);
+  while(j < n)
+   printf(" %d ", arr2[j++]);
+}
+ 
+/* Driver program to test above function */
+int main()
+{
+  int arr1[] = {1, 2, 4, 5, 6};
+  int arr2[] = {2, 3, 5, 7};
+  int m = sizeof(arr1)/sizeof(arr1[0]);
+  int n = sizeof(arr2)/sizeof(arr2[0]);
+  printUnion(arr1, arr2, m, n);
+  getchar();
+  return 0;
+}
+Run on IDE
+Time Complexity: O(m+n)
+
+Algorithm Intersection(arr1[], arr2[]):
+For Intersection of two arrays, print the element only if the element is present in both arrays.
+1) Use two index variables i and j, initial values i = 0, j = 0
+2) If arr1[i] is smaller than arr2[j] then increment i.
+3) If arr1[i] is greater than arr2[j] then increment j.
+4) If both are same then print any of them and increment both i and j.
+
+#include<stdio.h>
+ 
+/* Function prints Intersection of arr1[] and arr2[]
+   m is the number of elements in arr1[]
+   n is the number of elements in arr2[] */
+int printIntersection(int arr1[], int arr2[], int m, int n)
+{
+  int i = 0, j = 0;
+  while (i < m && j < n)
+  {
+    if (arr1[i] < arr2[j])
+      i++;
+    else if (arr2[j] < arr1[i])
+      j++;
+    else /* if arr1[i] == arr2[j] */
+    {
+      printf(" %d ", arr2[j++]);
+      i++;
+    }
+  }
+}
+ 
+/* Driver program to test above function */
+int main()
+{
+  int arr1[] = {1, 2, 4, 5, 6};
+  int arr2[] = {2, 3, 5, 7};
+  int m = sizeof(arr1)/sizeof(arr1[0]);
+  int n = sizeof(arr2)/sizeof(arr2[0]);
+  printIntersection(arr1, arr2, m, n);
+  getchar();
+  return 0;
+}
+Run on IDE
+Time Complexity: O(m+n)
