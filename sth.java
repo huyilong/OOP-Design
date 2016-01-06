27cfdffdd73c61b8d9b12fbd35a648b450fef1b5
hu.yilong <hu.yilong@wustl.edu>
2016-01-06 17:04:30 -0600

    SUMMARY - ALLLL ** Problems

--- sth.java
+++ sth.java
@@ -478,3 +478,16 @@ public class Solution {
         return x.isEmpty();         
     }
 }    
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        //由于数字在计算机是以二进制存储的，每位上都是0或1，如果我们把两个相同的数字异或，0与0异或是0,
+        //1与1异或也是0，那么我们会得到0。根据这个特点，我们把数组中所有的数字都异或起来，              
+        //则每对相同的数字都会得0，然后最后剩下来的数字就是那个只有1次的数字。
+        int res = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+        	res = res^nums[i];
+        }
+        return res;
+    }
+}
